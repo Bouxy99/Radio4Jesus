d4ed1cc2bc0d0cf328e80a70e885c0adb9e81076
Bouxy99 <62006954+Bouxy99@users.noreply.github.com>
2025-05-05 22:16:32 +0200

    Add Dieu si grand Remix

--- js/global.js
+++ js/global.js
@@ -24,7 +24,8 @@ const playlist = {
 const playlist_remix = {
     1: { id: 1.1, name: "Trouver son amour", file: "audio/remix/Trouver_son_amour_REMIX.mp3", date:'2025-05-01' },
     2: { id: 2.1, name: "Psaumes 33", file: "audio/remix/Psaumes_33_REMIX.mp3", date:'2025-05-04' },
-    3: { id: 4.1, name: "Un horizon meilleur", file: "audio/remix/Un_horizon_meilleur_REMIX.mp3", date:'2025-05-01' }
+    3: { id: 4.1, name: "Un horizon meilleur", file: "audio/remix/Un_horizon_meilleur_REMIX.mp3", date:'2025-05-01' },
+    4: { id: 5.1, name: "Dieu si grand", file: "audio/remix/Dieu_si_grand_REMIX.mp3", date:'2025-05-05' }
 }
 
 let activePlaylist = playlist;
